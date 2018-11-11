b38185e05675161c295b6ec033f6ac356c334d70
Rodolfo Borri <rodolfoborri@hotmail.com>
2018-11-11 18:08:28 -0200

    sintomas e doenças adicionadas

--- main.pl
+++ main.pl
@@ -3,10 +3,10 @@ sintoma('diarreia').
 sintoma('vomito').
 sintoma('pouco apetite').
 sintoma('garganta inflamada').
-sintome('dor de cabeca').
+sintoma('dor de cabeca').
+sintoma('hemorragia').
 sintoma('dor generalizada').
 sintoma('dor nas costas').
-sintoma('hemorragia').
 sintoma('tontura').
 sintoma('dificuldade respiratoria').
 sintoma('ja tive dengue').
@@ -16,6 +16,21 @@ sintoma('fadiga').
 sintoma('perda de peso').
 sintoma('dor no peito').
 sintoma('suor excessivo').
+sintoma('anemia').
+sintoma('nausea').
+sintoma('dor no estomago').
+sintoma('queimacao no estomago').
+sintoma('enjoo').
+sintoma('manchas avermelhadas no corpo').
+sintoma('coceira').
+sintoma('coriza').
+sintoma('dor no testiculo').
+sintoma('formigamento').
+sintoma('erupcoes cutaneas').
+sintoma('azia').
+sintoma('ulcera').
+sintoma('olhos amarelados').
+sintoma('pele amarelada').
 
 
 doenca('Dengue Hemorragica', A):-
@@ -23,12 +38,12 @@ doenca('Dengue Hemorragica', A):-
     member('vomito', A),
     member('dificuladde respiratoria',A),
     member('ja tive dengue', A),
-    member('tontura', A).
+    member('tontura', A),!.
 
 doenca('Dengue Classica', A):-
     member('febre', A),
     member('dor de cabeca', A),
-    member('dor generalizada',A).
+    member('dor generalizada',A),!.
 
 doenca('Tuberculose', A):-
     member('fadiga', A),
@@ -36,7 +51,7 @@ doenca('Tuberculose', A):-
     member('perda de peso', A),
     member('pouco apetite', A),
     member('febre', A),
-    member('calafrios', A).
+    member('calafrios', A),!.
 
 doenca('Pneumonia', A):-
     member('dor nas costas', A),
@@ -46,21 +61,118 @@ doenca('Pneumonia', A):-
     member('fadiga', A),
     member('febre', A),
     member('suor excessivo', A),
-    member('dificuldade respiratoria',A).
+    member('dificuldade respiratoria',A),!.
 
+doenca('Gastrite', A):-
+    member('dor no estomago', A),
+    member('queimacao no estomago', A),
+    member('enjoo', A),
+    member('pouco apetite', A),
+    member('vomito', A),!.
 
+doenca('Catapora', A):-
+    member('manchas avermelhadas no corpo', A),
+    member('fadiga', A),
+    member('febre', A),
+    member('garganta inflamada', A),
+    member('pouco apetite', A),!.
 
+doenca('Leptospirose', A):-
+    member('dor generalizada', A),
+    member('diarreia', A),
+    member('nausea', A),
+    member('vomito', A),
+    member('calafrios', A),
+    member('fadiga', A),
+    member('febre', A),!.
 
+doenca('Rubeola', A):-
+    member('coriza', A),
+    member('febre', A),
+    member('manchas avermelhadas no corpo', A),!.
 
+doenca('Caxumba', A):-
+    member('dor no testiculo', A),
+    member('fadiga', A),
+    member('febre', A),
+    member('pouco apetite', A),
+    member('dor de cabeca', A),!.
 
+doenca('Bronquite', A):-
+    member('tosse prolongada', A),
+    member('calafrios', A),
+    member('fadiga', A),
+    member('febre', A),
+    member('dificuldade respiratoria', A),
+    member('coriza', A),!.
 
+doenca('Sarampo', A):-
+    member('dor generalizada', A),
+    member('tosse prolongada', A),
+    member('fadiga', A),
+    member('febre', A),
+    member('pouco apetite', A),
+    member('manchas avermelhadas no corpo', A),!.
 
+doenca('Hanseniase', A):-
+    member('fadiga', A),
+    member('formigamento', A),
+    member('perda de peso', A),
+    member('dor generalizada', A),
+    member('manchas avermelhadas no corpo', A),
+    member('erupcoes cutaneas', A),!.
 
+doenca('Meningite', A):-
+    member('dor generalizada', A),
+    member('calafrios', A),
+    member('manchas avermelhadas no corpo', A),
+    member('erupcoes cutaneas', A),
+    member('fadiga', A),
+    member('febre', A),
+    member('dor de cabeca', A),
+    member('nausea', A),
+    member('vomito', A),!.
 
+doenca('H1N1', A):-
+    member('dor generalizada', A),
+    member('diarreia', A),
+    member('nausea', A),
+    member('vomito', A),
+    member('febre', A),
+    member('calafrios', A),
+    member('fadiga', A),!.
 
+doenca('Ataque Cardiaco', A):-
+    member('dor no peito', A),
+    member('fadiga', A),
+    member('tontura', A),
+    member('nausea', A),
+    member('vomito', A),
+    member('azia', A),!.
 
+doenca('Sifilis', A):-
+    member('dor generalizada', A),
+    member('fadiga', A),
+    member('febre', A),
+    member('pouco apetite', A),
+    member('ulcera', A),!.
 
+doenca('Herpes', A):-
+    member('formigamento', A),
+    member('erupcoes cutaneas', A),
+    member('manchas avermelhadas no corpo', A),
+    member('ulceras', A),!.
 
+doenca('Febre Amarela', A):-
+    member('dor generalizada', A),
+    member('calafrios', A),
+    member('fadiga', A),
+    member('febre', A),
+    member('pouco apetite', A),
+    member('nausea', A),
+    member('vomito', A),
+    member('olhos amarelados', A),
+    member('pele amarelada', A),!.
 
 
 
